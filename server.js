24fe5e9f112b074097337e929f907c3faf7e626d
sen9040 <62924580+sen9040@users.noreply.github.com>
2020-08-19 17:47:02 +0900

    server.js 추가

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/config.env" });
+
+// 내가 만든 파일 require는 이 아래에다가 넣자.
+const search = require("./routes/search");
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1/search", search);
+
+const PORT = process.env.PORT || 5776;
+
+app.listen(PORT, console.log("서버가동 !" + PORT));
