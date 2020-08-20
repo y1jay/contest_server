2b1ee48b8d970eb1e2971d5d2086c0c70f346bd2
sen9040 <62924580+sen9040@users.noreply.github.com>
2020-08-20 16:39:07 +0900

    메인 과 검색 api

--- controllers/search.js
+++ controllers/search.js
@@ -30,27 +30,132 @@ exports.getSearch = async (req, res, next) => {
   }
 };
 
-// @desc        통합 검색
-// @route       GET /api/v1/search/all?keyword=&offset=
+// @desc        sport 종목 별 main 화면용
+// @route       GET /api/v1/search/sports?keyword=&offset=
 // @request
 // @response    success, cnt, items[]
-exports.getAllsSearch = async (req, res, next) => {
+exports.getSports = async (req, res, next) => {
   let keyword = req.query.keyword;
+  let lat = req.query.lat;
+  let lng = req.query.lng;
   let offset = req.query.offset;
 
-  let query = `select * from search as s
-  left join sport_rows as t
-  on t.SVCID = s.S_SVCID
-  left join park_rows as p
-  on p.P_IDX = s.S_P_IDX
-  left join way_rows as w
-  on w.CPI_IDX = s.S_CPI_IDX
-  where t.SVCNM like "%${keyword}%" or p.P_NAME like "%${keyword}%"
-   or p.P_PARK like "%${keyword}%" or w.CPI_NAME like "%${keyword}%" 
-   or w.COURSE_NAME like "%${keyword}%" limit 0,${offset}`;
-
+  let query = `SELECT  s.*,  
+  ( 6371 * acos ( cos ( radians(${lat}) ) * cos( radians( s.X ) ) * 
+  cos( radians( s.Y ) - radians(${lng}) ) + sin ( radians(${lat}) ) * 
+  sin( radians( s.X ) ))) AS distance 
+  FROM sport_rows s 
+  where MINCLASSNM like "%${keyword}%"
+  HAVING distance < 100 ORDER BY distance LIMIT ${offset} ,25;`;
   try {
     [rows] = await connection.query(query);
+
+    res.status(200).json({ success: true, cnt: rows.length, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, e });
+  }
+};
+
+// @desc        park  main 화면용
+// @route       GET /api/v1/search/park?keyword=&offset=
+// @request
+// @response    success, cnt, items[]
+exports.getPark = async (req, res, next) => {
+  let keyword = req.query.keyword;
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  let offset = req.query.offset;
+
+  let query = `SELECT  s.*,  
+  ( 6371 * acos ( cos ( radians(${lat}) ) * cos( radians( s.LATITUDE ) ) * 
+  cos( radians( s.LONGITUDE ) - radians(${lng}) ) + sin ( radians(${lat}) ) * 
+  sin( radians( s.LATITUDE ) ))) AS distance 
+  FROM park_rows s 
+  HAVING distance < 100 ORDER BY distance LIMIT ${offset} ,25;`;
+  try {
+    [rows] = await connection.query(query);
+
+    res.status(200).json({ success: true, cnt: rows.length, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, e });
+  }
+};
+
+// @desc        way main 화면용
+// @route       GET /api/v1/search/way?keyword=&offset=
+// @request
+// @response    success, cnt, items[]
+exports.getWay = async (req, res, next) => {
+  let keyword = req.query.keyword;
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  let offset = req.query.offset;
+
+  let query = `SELECT  s.*,  
+  ( 6371 * acos ( cos ( radians(${lat}) ) * cos( radians( s.X ) ) * 
+  cos( radians( s.Y ) - radians(${lng}) ) + sin ( radians(${lat}) ) * 
+  sin( radians( s.X ) ))) AS distance 
+  FROM way_rows s 
+  HAVING distance < 100 ORDER BY distance LIMIT ${offset} ,25;`;
+
+  let testquery = `select * from way_rows limit ${offset},25`;
+
+  try {
+    [rows] = await connection.query(testquery);
+
+    res.status(200).json({ success: true, cnt: rows.length, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, e });
+  }
+};
+
+// @desc        park  main 화면용
+// @route       GET /api/v1/search/parksearch?keyword=&offset=
+// @request
+// @response    success, cnt, items[]
+exports.getParkSearch = async (req, res, next) => {
+  let keyword = req.query.keyword;
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  let offset = req.query.offset;
+
+  let query = `SELECT  s.*,  
+  ( 6371 * acos ( cos ( radians(${lat}) ) * cos( radians( s.LATITUDE ) ) * 
+  cos( radians( s.LONGITUDE ) - radians(${lng}) ) + sin ( radians(${lat}) ) * 
+  sin( radians( s.LATITUDE ) ))) AS distance 
+  FROM park_rows s 
+  where s.P_PARK like "%${keyword}%" or s.P_ZONE like "%${keyword}%"
+  HAVING distance < 100 ORDER BY distance LIMIT ${offset} ,25;`;
+  try {
+    [rows] = await connection.query(query);
+
+    res.status(200).json({ success: true, cnt: rows.length, items: rows });
+  } catch (e) {
+    res.status(500).json({ success: false, e });
+  }
+};
+
+// @desc        way main 화면용
+// @route       GET /api/v1/search/waysearch?keyword=&offset=
+// @request
+// @response    success, cnt, items[]
+exports.getWaySearch = async (req, res, next) => {
+  let keyword = req.query.keyword;
+  let lat = req.query.lat;
+  let lng = req.query.lng;
+  let offset = req.query.offset;
+
+  let query = `SELECT  s.*,  
+  ( 6371 * acos ( cos ( radians(${lat}) ) * cos( radians( s.X ) ) * 
+  cos( radians( s.Y ) - radians(${lng}) ) + sin ( radians(${lat}) ) * 
+  sin( radians( s.X ) ))) AS distance 
+  FROM way_rows s 
+  HAVING distance < 100 ORDER BY distance LIMIT ${offset} ,25;`;
+
+  let testquery = `select * from way_rows where COURSE_NAME like "%${keyword}%" or AREA_GU like "%${keyword}%" limit ${offset},25`;
+
+  try {
+    [rows] = await connection.query(testquery);
 
     res.status(200).json({ success: true, cnt: rows.length, items: rows });
   } catch (e) {
--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { getSearch, getAllsSearch } = require("../controllers/search");
+const {
+  getSearch,
+  getSports,
+  getPark,
+  getWay,
+  getWaySearch,
+  getParkSearch,
+} = require("../controllers/search");
 
 const router = express.Router();
 
 router.route("/").get(getSearch);
-router.route("/area").get(getAllsSearch);
+router.route("/sports").get(getSports);
+router.route("/park").get(getPark);
+router.route("/way").get(getWay);
+router.route("/parksearch").get(getParkSearch);
+router.route("/waysearch").get(getWaySearch);
 
 module.exports = router;
